f7f672092b2a6a269445e9a94c5bb6bcfc657bab
sinzinc28 <sinthana.kong28@gmail.com>
2021-07-13 21:04:47 +0700

    build

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+namespace InterviewTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proc = Process.Start(@"cmd.exe ",@"/c https://github.com/sinzinc28/InterviewTest/blob/main/formSearch.html");
+        }
+    }
+}
